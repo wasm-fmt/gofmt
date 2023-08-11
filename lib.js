bd4aafe03d625de790d707b04ca277ceb3147aba
magic-akari <akari.ccino@gmail.com>
2023-08-12 02:38:15 +0800

    refactor: early return for instance

--- lib.js
+++ lib.js
@@ -4,41 +4,37 @@ const go = new Go();
 let inst;
 
 export default async function init(wasm_url) {
-    if (!inst) {
-        if (!wasm_url) {
-            wasm_url = new URL("lib.wasm", import.meta.url);
-        }
-
-        if (typeof wasm_url === "string") {
-            wasm_url = new URL(wasm_url);
-        }
-
-        if (
-            typeof __webpack_require__ !== "function" &&
-            wasm_url.protocol === "file:"
-        ) {
-            inst = import("node:fs/promises")
-                .then((fs) => fs.readFile(wasm_url))
-                .then((bytes) =>
-                    WebAssembly.instantiate(bytes, go.importObject)
-                );
-        } else if ("instantiateStreaming" in WebAssembly) {
-            inst = WebAssembly.instantiateStreaming(
-                fetch(wasm_url),
-                go.importObject
-            );
-        } else {
-            inst = fetch(wasm_url)
-                .then((response) => response.arrayBuffer())
-                .then((bytes) =>
-                    WebAssembly.instantiate(bytes, go.importObject)
-                );
-        }
-        inst = (await inst).instance;
-        go.run(inst);
+    if (inst) {
+        return await inst;
     }
 
-    await inst;
+    if (!wasm_url) {
+        wasm_url = new URL("lib.wasm", import.meta.url);
+    }
+
+    if (typeof wasm_url === "string") {
+        wasm_url = new URL(wasm_url);
+    }
+
+    if (
+        typeof __webpack_require__ !== "function" &&
+        wasm_url.protocol === "file:"
+    ) {
+        inst = import("node:fs/promises")
+            .then((fs) => fs.readFile(wasm_url))
+            .then((bytes) => WebAssembly.instantiate(bytes, go.importObject));
+    } else if ("instantiateStreaming" in WebAssembly) {
+        inst = WebAssembly.instantiateStreaming(
+            fetch(wasm_url),
+            go.importObject
+        );
+    } else {
+        inst = fetch(wasm_url)
+            .then((response) => response.arrayBuffer())
+            .then((bytes) => WebAssembly.instantiate(bytes, go.importObject));
+    }
+    inst = (await inst).instance;
+    go.run(inst);
 }
 
 export function format(input) {
