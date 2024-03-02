03b9303098d5f8c86f523f4778e052a1ada20746
magic-akari <akari.ccino@gmail.com>
2024-03-02 21:43:52 +0800

    chore: Update TinyGo to version 0.31.1

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/wasm-fmt/gofmt
 
-go 1.21
+go 1.22
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,7 @@
 set -Eeo pipefail
 
 echo "Building..."
-tinygo build -o=gofmt.wasm -target=wasm -no-debug -stack-size=64kb ./src/lib.go
-
-if [[ ! -z "${WASM_OPT}" ]]; then
-	echo "Optimizing..."
-
-	tmp_dir=$(mktemp -d)
-	cp gofmt.wasm $tmp_dir/gofmt.wasm
-
-	wasm-opt gofmt.wasm -Os -o $tmp_dir/gofmt_os.wasm
-	wasm-opt gofmt.wasm -Oz -o $tmp_dir/gofmt_oz.wasm
-
-	smallest_wasm=$(ls -Sr $tmp_dir/*.wasm | head -1)
-
-	mv $smallest_wasm gofmt.wasm
-	rm -rf $tmp_dir
-fi
+tinygo build -o=gofmt.wasm -target=wasm -no-debug ./src/lib.go
 
 echo "Generating JS..."
 cp $(tinygo env TINYGOROOT)/targets/wasm_exec.js ./gofmt.js
