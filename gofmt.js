f57b112c09b3866ca9f0183f06602afe0bf034d2
magic-akari <akari.ccino@gmail.com>
2026-01-15 23:28:58 +0800

    refactor: remove unused initSync and initAsync functions from gofmt.js

--- gofmt.js
+++ gofmt.js
@@ -4,14 +4,6 @@ import { format as _format } from "./gofmt_binding.js";
 
 wasm._initialize();
 
-export function initSync() {
-	return wasm;
-}
-
-export default async function initAsync() {
-	return wasm;
-}
-
 export function format(source) {
 	return _format(wasm, source);
 }
